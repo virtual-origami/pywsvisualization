7362d72201bf9bbf3be591d77b244b241e971a2a
Karthik Shenoy <she@biba.uni-bremen.de>
2021-09-06 17:19:26 +0200

    refactoring changes
    Signed-off-by: Karthik Shenoy <she@biba.uni-bremen.de>

--- pywsvisualization/WSGui/WS.py
+++ pywsvisualization/WSGui/WS.py
@@ -96,64 +96,69 @@ class WS:
         :return: None
         """
         try:
-            if "rmt.robot." in binding_name:
-                # extract robot id
-                binding_delimited_array = binding_name.split(".")
-                robot_id = binding_delimited_array[len(binding_delimited_array) - 1]
-                message_body = json.loads(message_body)
-                msg_attributes = message_body.keys()
-                if ("id" in msg_attributes) and \
-                        ("shoulder" in msg_attributes) and \
-                        ("elbow" in msg_attributes) and \
-                        ("wrist" in msg_attributes) and \
-                        ("base" in msg_attributes):
-                    if robot_id == message_body["id"]:
-                        logger.debug(f'exchange: {exchange_name} msg: {message_body}')
-                        base = [message_body["base"][0], message_body["base"][1]]
-                        shoulder = [message_body["shoulder"][0], message_body["shoulder"][1]]
-                        elbow = [message_body["elbow"][0], message_body["elbow"][1]]
-                        wrist = [message_body["wrist"][0], message_body["wrist"][1]]
-                        self.robots.update(id=robot_id,
-                                           base=base,
-                                           shoulder=shoulder,
-                                           elbow=elbow,
-                                           wrist=wrist)
-                    else:
-                        return False  # robot id in binding name and message body does not match
-                else:
-                    return False  # invalid message body format
-            elif "plm.walker" in binding_name:
-                # extract walker id
-                binding_delimited_array = binding_name.split(".")
-                walker_id = binding_delimited_array[len(binding_delimited_array) - 1]
-                msg_attributes = message_body.keys()
-                if ("id" in msg_attributes) and \
-                        ("x_ref_pos" in msg_attributes) and \
-                        ("y_ref_pos" in msg_attributes) and \
-                        ("z_ref_pos" in msg_attributes) and \
-                        ("x_uwb_pos" in msg_attributes) and \
-                        ("y_uwb_pos" in msg_attributes) and \
-                        ("z_uwb_pos" in msg_attributes) and \
-                        ("x_ref_pos" in msg_attributes) and \
-                        ("y_ref_pos" in msg_attributes) and \
-                        ("z_ref_pos" in msg_attributes) and \
-                        ("timestamp" in msg_attributes):
-                    if walker_id == message_body["id"]:
-                        logger.debug(message_body)
-                        ref_position = [message_body["x_ref_pos"], message_body["y_ref_pos"]]
-                        uwb_position = [message_body["x_uwb_pos"], message_body["y_uwb_pos"]]
-                        est_position = [message_body["x_est_pos"], message_body["y_est_pos"]]
-                        self.particles.update(id=walker_id,
-                                              ref_position=ref_position,
-                                              uwb_position=uwb_position,
-                                              est_position=est_position,
-                                              radius=5,
-                                              world=message_body["view"],
-                                              ref_heading=message_body['ref_heading'])
-                    else:
-                        return False  # robot id in binding name and message body does not match
-                else:
-                    return False  # invalid message body format
+            for subscriber in self.subscribers:
+                if subscriber.exchange_name == exchange_name:
+                    if "visual.rmt.robot." in binding_name:
+                        # extract robot id
+                        binding_delimited_array = binding_name.split(".")
+                        robot_id = binding_delimited_array[len(binding_delimited_array) - 1]
+                        if type(message_body) is bytes:
+                            message_body = json.loads(message_body)
+                        msg_attributes = message_body.keys()
+                        if ("id" in msg_attributes) and \
+                                ("shoulder" in msg_attributes) and \
+                                ("elbow" in msg_attributes) and \
+                                ("wrist" in msg_attributes) and \
+                                ("base" in msg_attributes):
+                            if robot_id == message_body["id"]:
+                                logger.debug(f'exchange: {exchange_name} msg: {message_body}')
+                                base = [message_body["base"][0], message_body["base"][1]]
+                                shoulder = [message_body["shoulder"][0], message_body["shoulder"][1]]
+                                elbow = [message_body["elbow"][0], message_body["elbow"][1]]
+                                wrist = [message_body["wrist"][0], message_body["wrist"][1]]
+                                self.robots.update(id=robot_id,
+                                                   base=base,
+                                                   shoulder=shoulder,
+                                                   elbow=elbow,
+                                                   wrist=wrist)
+                            else:
+                                return False  # robot id in binding name and message body does not match
+                        else:
+                            return False  # invalid message body format
+                    elif "visual.plm.personnel" in binding_name:
+                        # extract walker id
+                        binding_delimited_array = binding_name.split(".")
+                        walker_id = binding_delimited_array[len(binding_delimited_array) - 1]
+                        if type(message_body) is bytes:
+                            message_body = json.loads(message_body)
+                        msg_attributes = message_body.keys()
+                        if ("id" in msg_attributes) and \
+                                ("x_ref_pos" in msg_attributes) and \
+                                ("y_ref_pos" in msg_attributes) and \
+                                ("z_ref_pos" in msg_attributes) and \
+                                ("x_uwb_pos" in msg_attributes) and \
+                                ("y_uwb_pos" in msg_attributes) and \
+                                ("z_uwb_pos" in msg_attributes) and \
+                                ("x_ref_pos" in msg_attributes) and \
+                                ("y_ref_pos" in msg_attributes) and \
+                                ("z_ref_pos" in msg_attributes) and \
+                                ("timestamp" in msg_attributes):
+                            if walker_id == message_body["id"]:
+                                logger.debug(message_body)
+                                ref_position = [message_body["x_ref_pos"], message_body["y_ref_pos"]]
+                                uwb_position = [message_body["x_uwb_pos"], message_body["y_uwb_pos"]]
+                                est_position = [message_body["x_est_pos"], message_body["y_est_pos"]]
+                                self.particles.update(id=walker_id,
+                                                      ref_position=ref_position,
+                                                      uwb_position=uwb_position,
+                                                      est_position=est_position,
+                                                      radius=5,
+                                                      world=message_body["view"],
+                                                      ref_heading=message_body['ref_heading'])
+                            else:
+                                return False  # robot id in binding name and message body does not match
+                        else:
+                            return False  # invalid message body format
         except AssertionError as e:
             logging.critical(e)
             exc_type, exc_value, exc_traceback = sys.exc_info()
